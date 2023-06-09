f6bfa83593d69a543fa8a2beed9cac6ec71f5257
xeno <kerasidisxenokratis@gmail.com>
2023-06-09 11:13:05 +0300

    creation of databases

---
+++ database/migrations/2023_06_09_075120_create_owners_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOwnersTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('owners', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('first_name')->nullable();
+            $table->string('surname')->nullable();
+            $table->string('email')->nullable();
+            $table->string('phone')->nullable();
+            $table->text('address')->nullable();
+            $table->timestamps();
+        });
+    }
+    public function down()
+    {
+        Schema::dropIfExists('owners');
+    }
+}
---
+++ database/migrations/2023_06_09_075142_create_animals_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAnimalsTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('animals', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('image_id')->nullable();
+            $table->unsignedBigInteger('owner_id')->nullable();
+            $table->string('name')->nullable();
+            $table->string('species')->nullable();
+            $table->string('breed')->nullable();
+            $table->integer('age')->nullable();
+            $table->integer('weight')->nullable();
+            $table->timestamps();
+
+            $table->foreign('owner_id')->references('id')->on('owners')->onDelete('cascade');
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('animals');
+    }
+}
