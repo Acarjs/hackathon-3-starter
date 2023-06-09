6603bc6d1b98498c4215044bce5fc30b652895c5
xeno <kerasidisxenokratis@gmail.com>
2023-06-09 11:24:38 +0300

    tables w return

--- database/migrations/2023_06_09_075120_create_owners_table.php
+++ database/migrations/2023_06_09_075120_create_owners_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateOwnersTable extends Migration
+return new class extends Migration
 {
     public function up()
     {
@@ -18,8 +18,9 @@ class CreateOwnersTable extends Migration
             $table->timestamps();
         });
     }
+
     public function down()
     {
         Schema::dropIfExists('owners');
     }
-}
+};
