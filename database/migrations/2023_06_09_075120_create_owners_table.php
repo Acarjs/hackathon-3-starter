0492ee18c587e162774e179de9544004c3d75cca
xeno <kerasidisxenokratis@gmail.com>
2023-06-09 12:11:24 +0300

    changes

--- database/migrations/2023_06_09_075120_create_owners_table.php
+++ database/migrations/2023_06_09_075120_create_owners_table.php
@@ -4,12 +4,12 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateOwnersTable extends Migration
 {
     public function up()
     {
         Schema::create('owners', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id');
             $table->string('first_name')->nullable();
             $table->string('surname')->nullable();
             $table->string('email')->nullable();
@@ -18,9 +18,4 @@ return new class extends Migration
             $table->timestamps();
         });
     }
-
-    public function down()
-    {
-        Schema::dropIfExists('owners');
-    }
-};
+}
